16f709d878f83325b06fa66353bfd336df74e626
Balajee Raghbendra <raghbeb@192-168-1-108.tpgi.com.au>
2020-01-09 21:19:22 +1100

    next js app

---
+++ .next/static/webpack/static/development/pages/about.js.2829a6b587615f350e65.hot-update.js
@@ -0,0 +1,41 @@
+webpackHotUpdate("static/development/pages/about.js",{
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_MyLayout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/MyLayout */ "./components/MyLayout.js");
+var _jsxFileName = "/Users/raghbeb/Documents/Home-Work/Code/hello-next/pages/about.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var about = function about() {
+  return __jsx(_components_MyLayout__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, __jsx("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, "This is about page."));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (about);
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.2829a6b587615f350e65.hot-update.js.map
---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Layout from '../components/MyLayout'
+
+const about = () => {
+  return (
+    <Layout>
+      <p>
+        This is about page.
+      </p>
+    </Layout>
+  )
+}
+
+export default about
